60e5738ec913b98d0ee26b5404f72396fec26f0c
Douglas Davis <ddavis@cern.ch>
2018-11-07 16:01:13 -0500

    fix testg

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -1,10 +1,10 @@
 import sys
 import numpy as np
 try:
-    import twanet.viz
+    import twaml.viz
 except ImportError:
     sys.path.append('.')
-    import twanet.viz
+    import twaml.viz
 
 
 def test_compare_dist():
@@ -13,8 +13,8 @@ def test_compare_dist():
     d2 = np.random.randn(10000)
     nh1 = np.histogram(d1,bins=bins)
     nh2 = np.histogram(d2,bins=bins)
-    fig, ax, h1, h2 = twanet.viz.compare_distributions(d1, d2, bins=bins,
-                                                       colors=['red','green'])
+    fig, ax, h1, h2 = twaml.viz.compare_distributions(d1, d2, bins=bins,
+                                                      colors=['red','green'])
     np.testing.assert_array_equal(nh1[0], h1[0])
     np.testing.assert_array_equal(nh2[0], h2[0])
 
@@ -22,6 +22,6 @@ def test_compare_dist():
     d2 = np.random.randn(1000)
     nh1 = np.histogram(d1)
     nh2 = np.histogram(d2,bins=nh1[1])
-    fig, ax, h1, h2 = twanet.viz.compare_distributions(d1, d2, ratio=False)
+    fig, ax, h1, h2 = twaml.viz.compare_distributions(d1, d2, ratio=False)
     np.testing.assert_array_equal(nh1[0], h1[0])
     np.testing.assert_array_equal(nh2[0], h2[0])
