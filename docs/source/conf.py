7505ef30704efec554f7da5bd05915e76bd19e6a
Douglas Davis <ddavis@cern.ch>
2018-11-07 16:33:54 -0500

    autodoc mocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -185,11 +185,13 @@ epub_exclude_files = ['search.html']
 # -- Extension configuration -------------------------------------------------
 
 autodoc_mock_imports = [
-    "keras",
-    "torch",
-    "uproot",
-    "tables",
-    "tensorflow"
+    'keras',
+    'torch',
+    'uproot',
+    'tables',
+    'tensorflow',
+    'pandas',
+    'matplotlib'
 ]
 
 autoclass_content = 'both'
