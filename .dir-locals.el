e3033966fdb7be2d4047e3765f373efd3cb72bb7
Douglas Davis <ddavis@cern.ch>
2018-12-04 15:36:34 -0500

    emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((python-mode . ((eval . (add-to-list 'eglot-server-programs '((python-mode)
+                                                               "~/Software/Python/anaconda3/envs/twaml/bin/pyls"))))))
