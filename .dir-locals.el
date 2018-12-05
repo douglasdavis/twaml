15c77e6ef927bb0049ada0f11242db3404b68c94
Douglas Davis <ddavis@cern.ch>
2018-12-05 14:25:45 -0500

    update dir-locals [skip travis]

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
 ((python-mode . ((eval . (add-to-list 'eglot-server-programs '((python-mode)
-                                                               "~/Software/Python/anaconda3/envs/twaml/bin/pyls"))))))
+                                                               "~/Software/Python/anaconda3/envs/twaml/bin/pyls")))
+                 (eval . (setq py-shell-name  "~/Software/Python/anaconda3/envs/twaml/bin/python"
+                               py-ipython-command "~/Software/Python/anaconda3/envs/twaml/bin/ipython"))
+                 )))
