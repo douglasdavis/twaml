c1c038a5a64ac965289112ed7c875cdd59297197
Douglas Davis <ddavis@cern.ch>
2019-01-24 00:33:18 -0500

    viz compare_dists updates

--- twaml/viz.py
+++ twaml/viz.py
@@ -21,6 +21,8 @@ def compare_distributions(
     titles: List[str] = ["dist1", "dist2"],
     colors: List[str] = ["C0", "C1"],
     ratio: bool = True,
+    weight1: Optional[np.ndarray] = None,
+    weight2: Optional[np.ndarray] = None,
     **subplots_kw,
 ):
     """Compare two histogrammed distributons with matplotlib
@@ -28,15 +30,19 @@ def compare_distributions(
     Parameters
     ----------
     dist1
-      Any mpl-histogrammable object (``np.ndarray``, ``pd.Series``, etc.)
+      any mpl-histogrammable object (``np.ndarray``, ``pd.Series``, etc.)
     dist2
-      Any mpl-histogrammable object (``np.ndarray``, ``pd.Series``, etc.)
+      any mpl-histogrammable object (``np.ndarray``, ``pd.Series``, etc.)
     bins: np.ndarray
-      Define the bin edges
+      define the bin edges
     titles: List[str]
-      Labels for the distributions
+      labels for the distributions
     ratio: bool
-      Add a ratio plot
+      add a ratio plot
+    weight1: Optional[np.ndarray]
+      weights associated with dist1
+    weight2: Optional[np.ndarray]
+      weights associated with dist2
     subplots_kw: Dict
       all additional keywords to send to ``matplotlib.pyplot.subplots``
 
@@ -63,10 +69,20 @@ def compare_distributions(
             **subplots_kw,
         )
         h1 = ax[0].hist(
-            dist1, bins=bins, histtype="step", label=titles[0], color=colors[0]
+            dist1,
+            bins=bins,
+            histtype="step",
+            label=titles[0],
+            color=colors[0],
+            weights=weight1,
         )
         h2 = ax[0].hist(
-            dist2, bins=h1[1], histtype="step", label=titles[1], color=colors[1]
+            dist2,
+            bins=h1[1],
+            histtype="step",
+            label=titles[1],
+            color=colors[1],
+            weights=weight2,
         )
         centers = np.delete(h1[1], [0]) - (np.ediff1d(h1[1]) / 2.0)
         ax[1].plot(centers, h1[0] / h2[0], "k-")
