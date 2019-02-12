e6226f2e0d2bb5fdd359f0290a89d3632060c65c
Doug Davis <ddavis@ddavis.io>
2019-02-11 20:02:04 -0500

    rework docs

--- twaml/__init__.py
+++ twaml/__init__.py
@@ -6,3 +6,7 @@ This python package contains modules for handling different machine
 learning requirements for the ATLAS Full Run II tW analysis.
 
 """
+
+from .data import dataset
+from .viz import compare_distributions
+from .viz import compare_columns
