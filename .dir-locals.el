207c09b6bfc48c304f31900ba1cde92335776c51
Doug Davis <ddavis@ddavis.io>
2019-04-24 22:41:11 -0400

    delete dir-locals.el

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-((python-mode . ((eglot-server-programs    . ((python-mode "~/Software/Python/anaconda3/envs/twaml/bin/pyls")))
-                 (company-backends         . (company-capf))
-                 (python-shell-interpreter . "~/Software/Python/anaconda3/envs/twaml/bin/python")
-                 (eval . (add-hook 'before-save-hook #'eglot-format-buffer))
-                 ;;(py-shell-name . "~/Software/Python/anaconda3/envs/twaml/bin/python")
-                 ;;(py-ipython-command . "~/Software/Python/anaconda3/envs/twaml/bin/ipython")
-                 )))
